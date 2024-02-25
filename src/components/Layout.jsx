0b5598cd5125abbcf609fd8ed2e9ca4a6ceb68e7
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-26 00:42:45 +0530

    update post1

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Nav from './Nav'
-import ReactGA from"react-ga"
+import { Analytics } from '@vercel/analytics/react';
 import NextTopLoader from 'nextjs-toploader';
 export default function Layout({ children }) {
   return (
@@ -9,6 +9,7 @@ export default function Layout({ children }) {
       <Nav />
       <main className='pt-28 md:px-24 px-5 dark'>
         {children}
+        <Analytics />
       </main>
     </>
   )
