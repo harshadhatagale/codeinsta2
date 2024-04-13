1d7efdddfa344fab24dc266b409d55d38eccec16
harshadhatagale <harshadhatagale6@gmail.com>
2024-04-13 14:54:19 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import { Poppins } from 'next/font/google'
 const poppins= Poppins({
     subsets:["latin"],
@@ -8,16 +9,16 @@ export default function Footer() {
     return (
         <footer className={`${poppins.className} bg-slate-700 space-y-10 w-full flex flex-col py-10 justify-center items-center`}>
             <div className='flex space-x-4'>
-                <a href="https://github.com/harshadhatagale"><img className=' w-10' src="github.png" alt="github" /></a>
-                <a href="https://www.instagram.com/er.dashing_harsha_?igsh=MXc0ZTg1cTU4N3FjNQ=="><img className='w-10' src="instagram.png" alt="github" /></a>
-                <a href="https://www.facebook.com/profile.php?id=61557501074733"><img className='w-10' src="facebook.png" alt="facebook" /></a>
-                <a href="https://twitter.com/game_harsh78073"><img className='w-10' src="twitter.png" alt="facebook" /></a>
+                <Link href="https://github.com/harshadhatagale"><img className=' w-10' src="github.png" alt="github" /></Link>
+                <Link href="https://www.instagram.com/er.dashing_harsha_?igsh=MXc0ZTg1cTU4N3FjNQ=="><img className='w-10' src="instagram.png" alt="github" /></Link>
+                <Link href="https://www.facebook.com/profile.php?id=61557501074733"><img className='w-10' src="facebook.png" alt="facebook" /></Link>
+                <Link href="https://twitter.com/game_harsh78073"><img className='w-10' src="twitter.png" alt="facebook" /></Link>
             </div>
             <div className='flex space-x-4'>
-                <a href="/">Home</a>
-                <a href="/blog">Blog</a>
-                <a href="/about">About Us</a>
-                <a href="/privacy_policy">Privacy Policy</a>
+                <Link href="/">Home</Link>
+                <Link href="/blog">Blog</Link>
+                <Link href="/about">About Us</Link>
+                <Link href="/privacy_policy">Privacy Policy</Link>
             </div>
             <div>
             <p>&copy; 2024 Code Insta. All rights reserved.</p>
