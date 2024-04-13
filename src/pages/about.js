85ffb567adca6ca5ba918710a231875b10d99d2a
harshadhatagale <harshadhatagale6@gmail.com>
2024-04-13 13:45:23 +0530

    privacy

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import HeadTag from '@/components/HeadTag'
 export default function about() {
     return (
         <>
-            <HeadTag title="About Us!" description="Explore comprehensive coding tutorials on our blog, where we demystify programming concepts, provide hands-on examples, and guide you through real-world projects. From beginner-friendly introductions to advanced coding challenges, our tutorials cover a wide range of programming languages and technologies" canUrl="https://www.codeinsta.tech/about" />
+            
             <div>about</div>
         </>
     )
--- src/pages/privacy_policy.js
+++ src/pages/privacy_policy.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import styles from "../styles/privacy.module.css"
+import HeadTag from '@/components/HeadTag'
 export default function privacy_policy() {
     return (
         <>
+        <HeadTag title="Privacy Policy" description="Explore comprehensive coding tutorials on our blog, where we demystify programming concepts, provide hands-on examples, and guide you through real-world projects. From beginner-friendly introductions to advanced coding challenges, our tutorials cover a wide range of programming languages and technologies" canUrl="https://www.codeinsta.tech/privacy_policy" />
             <div className={`${styles.content} pt-5 px-12 lg:px-12 flex-col flex justify-center items-center justify-items-center  pb-5`}>
                 <h1>Privacy Policy</h1>
 
@@ -11,26 +13,26 @@ export default function privacy_policy() {
                 <h2>Personal Information We Collect</h2>
                 <p>
 
-                    When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically-collected information as "Device Information".
+                    When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically-collected information as "Device Information&quot;.
 
                     We collect Device Information using the following technologies:</p>
 
                 <ul>
                     <li>
-                        "Cookies" are data files that are placed on your device or computer and often include an anonymous unique identifier. For more information about cookies, and how to disable cookies, visit http://www.allaboutcookies.org.
+                        &quot;Cookies&quot; are data files that are placed on your device or computer and often include an anonymous unique identifier. For more information about cookies, and how to disable cookies, visit http://www.allaboutcookies.org.
                     </li>
                     <li>
-                        "Log files" track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.
+                        &quot;Log files&quot; track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.
                     </li>
                     <li>
-                        "Web beacons", "tags", and "pixels" are electronic files used to record information about how you browse the Site.
+                        &quot;Web beacons&quot;, &quot;tags&quot;, and &quot;pixels&quot; are electronic files used to record information about how you browse the Site.
                     </li>
                 </ul>
 
-                <p> Additionally, when you make a purchase or attempt to make a purchase through the Site, we collect certain information from you, including your name, billing address, shipping address, payment information "including credit card numbers", email address, and phone number. We refer to this information as "Order Information".</p>
+                <p> Additionally, when you make a purchase or attempt to make a purchase through the Site, we collect certain information from you, including your name, billing address, shipping address, payment information &quot;including credit card numbers&quot;, email address, and phone number. We refer to this information as &quot;Order Information&quot;.</p>
                 <h2>How We Use Your Personal Information</h2>
 
-                <p>We use the Order Information that we collect generally to fulfill any orders placed through the Site "including processing your payment information, arranging for shipping, and providing you with invoices and/or order confirmations". Additionally, we use this Order Information to:</p>
+                <p>We use the Order Information that we collect generally to fulfill any orders placed through the Site &quot;including processing your payment information, arranging for shipping, and providing you with invoices and/or order confirmations&quot;. Additionally, we use this Order Information to:</p>
                 <ul>
                     <li>
                         Communicate with you;
@@ -43,7 +45,7 @@ export default function privacy_policy() {
                     </li>
                 </ul>
 
-                <p>We use the Device Information that we collect to help us screen for potential risk and fraud "in particular, your IP address", and more generally to improve and optimize our Site "for example, by generating analytics about how our customers browse and interact with the Site, and to assess the success of our marketing and advertising campaigns".</p>
+                <p>We use the Device Information that we collect to help us screen for potential risk and fraud &quot;in particular, your IP address&quot;, and more generally to improve and optimize our Site &quot;for example, by generating analytics about how our customers browse and interact with the Site, and to assess the success of our marketing and advertising campaigns&quot;.</p>
 
                 <h2>Sharing Your Personal Information</h2>
 
@@ -56,7 +58,7 @@ export default function privacy_policy() {
 
                 If you are a European resident, you have the right to access personal information we hold about you and to ask that your personal information be corrected, updated, or deleted. If you would like to exercise this right, please contact us through the contact information below.
 
-                Additionally, if you are a European resident, we note that we are processing your information in order to fulfill contracts we might have with you "for example if you make an order through the Site", or otherwise to pursue our legitimate business interests listed above. Additionally, please note that your information will be transferred outside of Europe, including to Canada and the United States.
+                Additionally, if you are a European resident, we note that we are processing your information in order to fulfill contracts we might have with you &quot;for example if you make an order through the Site&quot;, or otherwise to pursue our legitimate business interests listed above. Additionally, please note that your information will be transferred outside of Europe, including to Canada and the United States.
 
                 <h2>Data Retention</h2>
 
