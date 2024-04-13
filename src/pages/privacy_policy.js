04b97985724b110c315c128122622415118ec428
harshadhatagale <harshadhatagale6@gmail.com>
2024-04-13 13:46:50 +0530

    privacy

--- src/pages/privacy_policy.js
+++ src/pages/privacy_policy.js
@@ -13,7 +13,7 @@ export default function privacy_policy() {
                 <h2>Personal Information We Collect</h2>
                 <p>
 
-                    When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically-collected information as "Device Information&quot;.
+                    When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically-collected information as &quot;Device Information&quot;.
 
                     We collect Device Information using the following technologies:</p>
 
