31f0baf6d2c196f1ba943b314e80d7f7d2deb672
harshadhatagale <harshadhatagale6@gmail.com>
2024-04-13 15:03:37 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const poppins= Poppins({
 })
 export default function Footer() {
     return (
-        <footer className={`${poppins.className} bg-slate-700 space-y-10 w-full flex flex-col py-10 justify-center items-center`}>
+        <footer className={`${poppins.className} bg-slate-200 dark:bg-slate-700 space-y-10 w-full flex flex-col py-10 justify-center items-center`}>
             <div className='flex space-x-4'>
                 <Link href="https://github.com/harshadhatagale"><img className=' w-10' src="github.png" alt="github" /></Link>
                 <Link href="https://www.instagram.com/er.dashing_harsha_?igsh=MXc0ZTg1cTU4N3FjNQ=="><img className='w-10' src="instagram.png" alt="github" /></Link>
